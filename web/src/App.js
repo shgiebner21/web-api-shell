ab9cd8af327bda4e8b8891ccc1121b18e4ca5320
Stephen Giebner <shgiebner21@gmail.com>
2018-03-21 09:28:49 -0400

    cleaned up project of clutter

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
-import Login from './pages/login'
-import Pharmacies from './pages/pharmacies'
-import Pharmacy from './pages/pharmacy'
 
 
 class App extends Component {
@@ -12,9 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <main>
-            <Route exact path='/pharmacies' component={Pharmacies} />
             <Route exact path='/' component={Login} />
-            Route exact path='/pharmacy' component={Pharmacy} />
 
           </main>
           {}
